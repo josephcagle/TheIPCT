fd4d20dae8d60a2671f0156fa23b02c2655a9d1f
Joseph Cagle <41763158+DarthFloopy@users.noreply.github.com>
2018-09-14 14:34:37 -0400

    Update install-server.bash

--- install-server.bash
+++ install-server.bash
@@ -1,5 +1,15 @@
-if [[ ! -z $(echo $PATH | grep '/home/.*/bin') ]]; then
-    echo << EOF | cat > ~/bin/ipct-server
+mkdir -p ~/bin
+if [[ -z $(echo $PATH | grep '/home/.*/bin') ]]; then
+    if [[ -f ~/.bash_profile ]]; then
+        echo "\nexport PATH=$USER/bin:$PATH" >> ~/.bash_profile
+    else
+        echo "\nexport PATH=$USER/bin:$PATH" >> ~/.profile
+    fi
+fi   
+echo << EOF | cat > ~/bin/ipct-server
 echo testing a113
 EOF
-fi
+
+
+
+chmod u+x ~/bin/ipct-server
